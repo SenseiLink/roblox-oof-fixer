91742dd352bf9497bffef82df9f7eb8ec170d049
SenseiLink <102203687+SenseiLink@users.noreply.github.com>
2022-07-27 20:47:44 -0400

    Update Patcher.bat

--- Patcher.bat
+++ Patcher.bat
@@ -2,4 +2,4 @@
 cd %USERPROFILE%\AppData\Local\Roblox\Versions
 FOR /F "delims=" %%i IN ('dir /b /ad-h /t:w /od') DO SET a=%%i
 cd %a%\content\sounds
-curl "https://github.com/SenseiLink/roblox-oof/blob/main/ouch.ogg"
+curl "https://github.com/SenseiLink/roblox-oof-fixer/blob/main/ouch.ogg"
